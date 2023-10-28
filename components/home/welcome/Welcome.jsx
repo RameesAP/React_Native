843d3718ba4a0eec96f9f774dabca8879bb55d3a
RameesAP <rameeswork@gmail.com>
2023-10-28 14:15:55 +0530

    horizotal

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,12 +11,13 @@ import { router, useRouter } from "expo-router";
 
 import styles from "./welcome.style";
 
-import { icons, SIZE } from "../../../constants";
+import { icons, SIZE, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
       <View style={styles.container}>
@@ -41,15 +42,22 @@ const Welcome = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.tabsContainer}>
-        <FlatList 
-        data={jobTypes}
-        renderItem={({item})=>(
-          <TouchableOpacity
-          style={styles.tab(activeJobType,item)}
-          >
-            <Text>{item}</Text>
-          </TouchableOpacity>
-        )}
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
     </View>
